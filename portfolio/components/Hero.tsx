cf2be22cbd623b2d44c4cc7390ac6481453b484b
farhanqamar <farhanqamarseo339@gmail.com>
2024-10-23 22:56:33 +0500

    first commit

---
+++ portfolio/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div>
+      finaly i have started my personal portfolio............. okay
+    </div>
+  )
+}
+
+export default Hero;
